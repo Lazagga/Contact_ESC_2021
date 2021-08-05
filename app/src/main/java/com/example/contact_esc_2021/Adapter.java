8a3530920476840361b363c210ee650dfbd82115
Lazagga <030126evan@gmail.com>
2021-08-05 20:39:03 +0900

    second week

---
+++ app/src/main/java/com/example/contact_esc_2021/Adapter.java
@@ -0,0 +1,119 @@
+package com.example.contact_esc_2021;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.DialogTitle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.Holder> {
+
+    private Context context;
+    private ArrayList<Contact> datalist;
+
+    public Adapter(Context context, ArrayList<Contact> datalist){
+        this.context = context;
+        this.datalist = datalist;
+    }
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
+        Holder holder = new Holder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull Adapter.Holder holder, int position) {
+
+        Contact contact = datalist.get(position);
+        holder.name.setText(contact.getName());
+        holder.phoneNum.setText(contact.getPhoneNumber());
+
+        holder.call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("전화/문자");
+                builder.setMessage("전화를 걸지 문자를 보낼지 선택해 주십시오.");
+                builder.setNeutralButton("전화", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String number = datalist.get(position).getPhoneNumber();
+                        Uri U_number = Uri.parse("tel:"+number);
+                        Intent call = new Intent(Intent.ACTION_CALL, U_number);
+                        context.startActivity(call.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                    }
+                });
+                builder.setPositiveButton("문자", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri U_sms = Uri.parse("tel:" + contact.getPhoneNumber());
+                        Intent intent = new Intent(Intent.ACTION_VIEW, U_sms);
+                        intent.putExtra("address", contact.getPhoneNumber());
+                        intent.putExtra("smsBody", "");
+                        intent.setType("vnd.android-dir/mms-sms");
+                        context.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                    }
+                });
+                builder.create().show();
+            }
+        });
+
+        holder.itemContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("삭제");
+                builder.setMessage("정말 이 연락처를 삭제하시겠습니까?");
+                builder.setNegativeButton("아니오", null);
+                builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //TODO : 핸드폰에서 연락처 지우기
+                        datalist.remove(position);
+                        notifyItemRemoved(position);
+                        notifyItemRangeChanged(position, datalist.size());
+                    }
+                });
+                builder.create().show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return datalist.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+
+        protected ConstraintLayout itemContainer;
+        protected TextView name;
+        protected TextView phoneNum;
+        protected Button call;
+
+        public Holder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
+            super(itemView);
+
+            itemContainer = itemView.findViewById(R.id.ItemContainer);
+            name = itemView.findViewById(R.id.Name);
+            phoneNum = itemView.findViewById(R.id.PhoneNumber);
+            call = itemView.findViewById(R.id.call);
+        }
+    }
+}
